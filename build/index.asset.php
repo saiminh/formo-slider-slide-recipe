4a54c2f5a7a9fed3788955ad0bed9685822615ca
saiminh <saiminh@gmail.com>
2024-05-28 19:30:36 +0200

    bring cheesename to front

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '10bb7ffeb70ca8c32939');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '317be9ddb01bbf0726b5');
